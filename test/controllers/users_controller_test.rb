ed9752f7fa19c097d44675328fbafbabaca92080
Hiroaki Ito <pigaru.gooood.mar19@gmail.com>
2017-11-04 06:36:16 +0000

    10.63

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
-def setup
+  def setup
     @user = users(:michael)
     @other_user = users(:archer)
   end
@@ -42,6 +42,21 @@ def setup
     patch user_path(@user), params: { user: { name: @user.name,
                                               email: @user.email } }
     assert flash.empty?
+    assert_redirected_to root_url
+  end
+  
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
     assert_redirected_to root_url
   end
   
